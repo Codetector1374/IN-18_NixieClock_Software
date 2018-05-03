05faff1915ec1d55695448287355b5b367917899
Codetector <codetector@codetector.cn>
2018-05-03 16:37:43 -0400

    Working on RTC

--- Lib/ds3231.h
+++ Lib/ds3231.h
@@ -5,4 +5,12 @@
 #ifndef IN18_NIXIECLOCK_DS3231_H
 #define IN18_NIXIECLOCK_DS3231_H
 
+#define DS3231_I2C_Address 0b1101000
+
+struct rtctime_t {
+    uint8_t hour;
+    uint8_t minutes;
+    uint8_t second;
+};
+
 #endif //IN18_NIXIECLOCK_DS3231_H
